601df8abb81b631bda7d7d5963dd6418bf43a00b
nayabjahan <nayabzafir@gmail.com>
2022-12-08 01:23:33 +0530

    Refactored:Refactor All Three To One Generic Method

--- src/main/java/com/blz/generics/Main.java
+++ src/main/java/com/blz/generics/Main.java
@@ -2,10 +2,17 @@ package com.blz.generics;
 
 public class Main {
     public static void main(String[] args) {
-        new MaxData(5, 6, 7).maxElement();
+        MaxData.maxElement(5, 6, 7);
+        MaxData.maxElement(8, 10, 4);
+        MaxData.maxElement(20, 9, 6);
         System.out.println("");
-        new MaxData(5.4f, 6.7f, 7.9f).maxElement();
+        MaxData.maxElement(5.4f, 6.7f, 7.9f);
+        MaxData.maxElement(8.7f, 10.2f, 4.4f);
+        MaxData.maxElement(20.1f, 9.2f, 6.9f);
         System.out.println("");
-        new MaxData("Ajay", "Abhishek", "Arif").maxElement();
+        MaxData.maxElement("Ajay", "Abhishek", "Arif");
+        MaxData.maxElement("Mohammad", "Rajdeep", "Jaydeep");
+        MaxData.maxElement("Kaushal", "Jitendra", "Ashkar");
+    
     }
 }
--- src/main/java/com/blz/generics/MaxData.java
+++ src/main/java/com/blz/generics/MaxData.java
@@ -1,18 +1,11 @@
 package com.blz.generics;
 
-public class MaxData <T extends Comparable<T>> {
-    T max, a, b, c;
-    MaxData(T a, T b, T c) {
-        this.a = a;
-        this.b = b;
-        this.c = c;
-    }
-    public void maxElement() {
-        max = a;
+public class MaxData {
+    public static <T extends Comparable <T>> void maxElement(T a, T b, T c) {
+        T max = a;
         if (b.compareTo(max) > 0)
             max = b;
         if (c.compareTo(max) > 0)
             max = c;
         System.out.println("Maximum Integer Value is : " + max);
-    }
-}
+    }}
