619fb240a75f6efa9fa0fedba949ffd88e5d139f
nayabjahan <nayabzafir@gmail.com>
2022-12-08 00:41:30 +0530

    Added:Maximum Of Three Float Numbers

--- src/main/java/com/blz/generics/Main.java
+++ src/main/java/com/blz/generics/Main.java
@@ -2,7 +2,7 @@ package com.blz.generics;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Generics Problem");
         MaxData.maxElement(5,25,30);
+        MaxData.maxFloat(2.1f,5.6f,8.6f);
     }
 }
--- src/main/java/com/blz/generics/MaxData.java
+++ src/main/java/com/blz/generics/MaxData.java
@@ -10,4 +10,12 @@ public class MaxData{
         System.out.println("maximum value is :"+ max);
 
     }
+    public static void maxFloat(Float a, Float b, Float c) {
+        Float max = a;
+        if (b.compareTo(max) > 0)
+            max = b;
+        if (c.compareTo(max) > 0)
+            max = c;
+        System.out.println("Maximum Float Value is : " + max);
+    }
 }
